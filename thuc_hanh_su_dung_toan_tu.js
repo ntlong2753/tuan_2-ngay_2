2491c835f1a291fbfd048dff4bca34a75ee8c4fb
nguyenthanhlongvn03 <nguyenthanhlongvn03@gmail.com>
2025-11-11 10:29:18 +0700

    add

---
+++ thuc_hanh_su_dung_toan_tu.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt('inputwidth');
+inputheight = prompt('inputheight');
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write('The area is: ' + area);
